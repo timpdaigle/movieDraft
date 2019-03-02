f6f634e57f7e0f22b05a4d565826e3843c9559d0
daiglet <timpd77@aol.com>
2019-03-02 13:31:57 -0500

    first commit

---
+++ app2.py
@@ -0,0 +1,218 @@
+#https://community.plot.ly/t/two-graphs-side-by-side/5312/2
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+# import dash_table
+from dash.dependencies import Input, Output
+import pandas as pd
+# import draftFunctions as dF
+# import head2Head.head2Head as h2h
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+import numpy as np
+import datetime
+import chartFunctions as cF
+
+draft = cF.loadResults(nameF='BoxOfficeDraft')
+boxDict = cF.loadResults(nameF='Head2Head')
+yearVals = list(draft.keys())
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config['suppress_callback_exceptions']=True
+#application = app.server
+app.title = 'Movie Draft Tracking and Results'
+app.layout = html.Div([
+	html.H1('Movie Draft Tracking and Results'),
+	dcc.Tabs(id='draftTabs', value='singleYr', children=[
+		dcc.Tab(label='Single Year Analysis', value='singleYr'),
+		dcc.Tab(label='Multiple Year Analysis', value='multiYr')
+	]),
+	html.Div(id='draftTabsContent')
+])
+
+@app.callback(Output('draftTabsContent', 'children'),
+	[Input('draftTabs', 'value')])
+def render_content(tab):
+	if tab == 'singleYr':
+		return html.Div([
+			html.Div([
+				html.H2('Single Year Analysis'),
+				html.Label('Choose Draft Year: '),
+				dcc.Dropdown(
+					id='year-dropdown',
+					options=[{'label': str(year), 'value': str(year)} for year in yearVals],
+					value=str(max(yearVals))
+			    )
+			]),
+			html.Div([
+				html.Div([
+					html.H3('Summary Table'),
+					dcc.Graph(id='aggTable', style={'height':400})
+				], className='six columns'),
+				html.Div([
+					html.H3('Stacked Actuals'),
+					dcc.Graph(id='graph-with-slider2', style={'height':400, 'border': '1px solid #333'})	
+				], className='six columns')
+			], className='row'),
+
+			html.Div([
+				html.H3('Cumulative Spliced'),
+				dcc.Graph(id='graph-with-slider1', style={'height':400, 'border': '1px solid #333'})
+			])
+		])
+	elif tab == 'multiYr':
+		return html.Div([
+			html.Div([
+				html.H2('Multiple Year Analysis'),
+				html.Label('Choose Draft Year(s): '),
+				dcc.Dropdown(
+					id='year-multichoice',
+					options=[{'label': str(year), 'value': str(year)} for year in yearVals],
+					value=[max(yearVals)],
+					multi=True
+				)
+			]),
+
+			html.Div([
+				html.Div([
+					html.H3('Space Holder')
+				], className='six columns'),
+				html.Div([
+					html.H3('Round Pick Distribution'),
+					dcc.Graph(id='boxWhisk', style={'height':400})
+				], className='six columns')
+			], className='row')
+		])
+
+@app.callback(
+	dash.dependencies.Output('graph-with-slider1', 'figure'),
+	[dash.dependencies.Input('year-dropdown', 'value')])
+def update_figure_1(selected_year):
+	yr = int(selected_year)
+	cumeInput = cF.userCume(boxDict,yr)
+	cumeCharts = dict()
+	cumeData = []
+	for i in cumeInput.keys():
+		cumeCharts[i] = sorted(np.array(cumeInput[i].items()).tolist())
+		xData,yDataLabel = zip(*cumeCharts[i])
+		yData,cumeLabel = zip(*yDataLabel)
+		fig = go.Scatter(x=xData,
+				y=yData,
+				mode='lines+markers',
+				marker=dict(size='2'),
+				text=cumeLabel,
+				name=i)
+		cumeData.append(fig)
+	now = datetime.datetime.now()
+	if now.year == yr:
+		#asof = 'as of '+now.strftime('%b-%d-%Y')
+		vLine= [
+		# Line Vertical
+			{'type': 'line',
+			'x0': now,
+			'y0': 0,
+			'x1': now,
+			'y1': 600,
+			'line': {
+			'color': 'black',
+			'width': 3,
+			},
+		}]
+	else:
+		asof = str(selected_year)
+		vLine=list()
+	cumeLayout= go.Layout(legend=dict(x=0, y=1),
+		shapes= vLine,
+		margin={'t':25, 'b':20})
+	return {
+		'data': cumeData, 
+		'layout': cumeLayout
+	}
+
+@app.callback(
+	dash.dependencies.Output('graph-with-slider2', 'figure'),
+	[dash.dependencies.Input('year-dropdown', 'value')])
+def update_figure_2(selected_year):
+	yr = int(selected_year)
+	roundDict = cF.segVals('round',['title','actual','id'],draft,yr)
+	stack = []
+	for i in roundDict.keys():
+	    stackInp = go.Bar(x=roundDict[i]['id'], 
+	        y=roundDict[i]['actual'], 
+	        text=roundDict[i]['title'], 
+	        name=str(i))
+	    stack.append(stackInp)
+	stackLayout = go.Layout(barmode='stack',
+		margin={'t':10, 'b':15, 'l':35})
+	stackFig = go.Figure(data=stack, layout=stackLayout)
+	return {
+		'data': stack, 
+		'layout': stackLayout
+	}
+
+@app.callback(
+	dash.dependencies.Output('boxWhisk', 'figure'),
+	[dash.dependencies.Input('year-multichoice', 'value')])
+def update_box_whisk(selectedYrs):
+	if selectedYrs == None:
+		return
+	draftDict = dict()
+	for i in selectedYrs:
+		draftDict[i] = cF.segVals('round',['actual'],draft,int(i))
+	boxWhisk = []
+	combDict = dict()
+	for i in draftDict.keys():
+		for z in draftDict[i].keys():
+			if z in combDict.keys():
+				combDict[z] = combDict[z]+draftDict[i][z]['actual']
+			else:
+				combDict[z] = draftDict[i][z]['actual']
+
+	for i in combDict.keys():
+	    tempInp = go.Box(y=combDict[i], 
+	        name='Round '+str(i), 
+	        boxpoints = 'all',
+	        marker=dict(size=5), 
+	        line=dict(width=2))
+	    boxWhisk.append(tempInp) 
+	boxWhiskLayout = go.Layout(showlegend=False,
+		margin={'t':10, 'b':25, 'l':35})
+	#boxWhiskFig = go.Figure(data=boxWhisk, layout=boxWhiskLayout)
+	return {
+		'data': boxWhisk,
+		'layout': boxWhiskLayout
+	}
+
+@app.callback(
+	dash.dependencies.Output('aggTable', 'figure'),
+	[dash.dependencies.Input('year-dropdown', 'value')])
+def get_agg_table(selected_year):
+	yr = int(selected_year)
+	#get aggregate values of actual and projected
+	aggIdAct = cF.uniqSum('id', 'act', boxDict, yr)
+	#get number observed/spliced values (actual where obs/projected otherwise)
+	splicedInfo = cF.getSplice('id',boxDict,yr)
+	actPanda = pd.DataFrame(aggIdAct, index = ['Actual'])
+	splPanda = pd.DataFrame(splicedInfo)
+	tableApp = actPanda.append(splPanda)
+	tableInp = tableApp.T
+	tableInp = tableInp.round({'Actual':2, 'total':2})
+	tableInp['observed'] = tableInp['observed'].astype(int)
+	tableInp.reset_index(level=0, inplace=True)
+	tableInp.sort_values(by='Actual', inplace=True, ascending=False)
+	idDict = {'index' : 'Participant', 
+	    'Actual' : 'Actual, Mil.$', 
+	    'total' : 'Spliced, Mil.$',
+	    'observed' : 'Picks Observed'}
+	tableInp.columns=[idDict.get(x, x) for x in tableInp.columns]
+	rows = tableInp.shape[0]+1
+	hCon = 375.0/rows
+	fullTable = ff.create_table(tableInp, height_constant=hCon)
+	return fullTable
+
+
+if __name__ == '__main__':
+	#application.run_server(debug=True)
+	app.run_server(debug=True)
---
+++ chartFunctions.py
@@ -0,0 +1,190 @@
+
+
+def loadResults(nameF,
+	location='data'):
+	import os
+	import pickle
+	nameF = nameF+'.p'
+	inPickPath = os.path.join(location, nameF)
+	print("load file: "+inPickPath)
+	return pickle.load(open(inPickPath, 'rb'))
+
+#return dataframe/dict/list containing listed fields
+def getFields(fields, dictName, year, outType='pandas'):
+	import pandas as pd
+	import numpy as np
+	seriesDict = dict()
+	titleKeys = [key for key in dictName[year].keys()]
+	for field in fields:
+		#print("add "+field+" to "+outType)
+		seriesDict[field] = []
+		for entry in titleKeys:
+			#if statement for title
+			if field in ['title']:
+				seriesDict[field].append(entry)
+			#else if statement for box office fields
+			elif field in ['actual','projected','model', 'act','bop','tim','al']:
+				boxField = 'boxOffice'
+				if field in ['act','bop','tim','al']:
+					boxField = 'box'
+				if field in dictName[year][entry][boxField].keys():
+					if boxField == 'boxOffice':
+						seriesDict[field].append(dictName[year][entry][boxField][field][-1])
+					else:
+						seriesDict[field].append(dictName[year][entry][boxField][field])
+				else:
+					seriesDict[field].append(float('NaN'))
+			#else for rest of the fields
+			else:			
+				seriesDict[field].append(dictName[year][entry][field])
+	if outType=='pandas':
+		return pd.DataFrame(seriesDict)
+	elif outType=='dict':
+		return seriesDict
+	elif outType=='list':
+		return list(zip(*seriesDict.values()))
+
+#return a dictionary of unique values obs for a given field
+def getUniq(field, dictName, year, defval=0):
+	dictUniq = dict()
+	for key in dictName[year].keys():
+		idg = dictName[year][key][field]
+		if str(idg) != 'na':
+			if idg not in dictUniq.keys():
+				#print(str(idg)+" added to unique entry list")
+				if defval == 0:
+					dictUniq[idg]=defval
+				elif defval == 'dict':
+					dictUniq[idg]=dict()
+				elif defval == 'list':
+					dictUniq[idg]=[]
+	return dictUniq
+
+#return a dictionary of unique value's summed values
+def uniqSum(field, metric, dictName, year):
+	sumDict = getUniq(field, dictName, year)
+	for entry in dictName[year].keys():
+		group = dictName[year][entry][field]
+		if group in sumDict.keys():
+			if metric in dictName[year][entry]['box'].keys():
+				value = dictName[year][entry]['box'][metric] / 1000000
+				#print(entry + ": "+str(value)+" added to "+field+" "+str(group))
+				sumDict[group] = sumDict[group] + value
+	return sumDict
+
+#return a dictionary containing segment specific total spliced values
+def getSplice(segment, dictName, year):
+	import numpy as np
+	spliceDict = getUniq(segment, dictName, year, defval='dict')
+	for user in spliceDict.keys():
+		spliceDict[user]['observed']=0
+		spliceDict[user]['total']=0
+	for entry in dictName[year].keys():
+		user = dictName[year][entry][segment]
+		if user in spliceDict.keys():
+			if 'act' in dictName[year][entry]['box'].keys():
+				spliceDict[user]['observed'] = spliceDict[user]['observed'] + 1
+				actual = dictName[year][entry]['box']['act'] / 1000000
+				spliceDict[user]['total']=spliceDict[user]['total'] + actual
+			elif 'delayed' in dictName[year][entry].keys():
+				spliceDict[user]['total']=spliceDict[user]['total'] + 0
+			elif 'bop' in dictName[year][entry]['box'].keys():
+				proj = dictName[year][entry]['box']['bop'] / 1000000
+				spliceDict[user]['total']=spliceDict[user]['total'] + proj
+			else:
+				box = np.mean([dictName[year][entry]['box']['al'], 
+					dictName[year][entry]['box']['tim']])/1000000
+				spliceDict[user]['total']=spliceDict[user]['total'] + box
+	return spliceDict
+
+#return a dictionary that contains keys based on unique values of segment and values that are lists
+def segVals(segment, metric, dictName, year):
+	valDict = getUniq(segment, dictName, year, defval='dict')
+	for m in metric:
+		for entry in dictName[year].keys():
+			group = dictName[year][entry][segment]
+			if m not in valDict[group].keys():
+				valDict[group][m]=[]
+			if m == 'title':
+				valDict[group][m].append(entry)
+			#else if statement for box office fields
+			elif m in ['actual', 'projected', 'model']:
+				if m in dictName[year][entry]['boxOffice'].keys():
+					valDict[group][m].append(dictName[year][entry]['boxOffice'][m][-1])
+				else:
+					valDict[group][m].append(float('NaN'))
+			#else for rest of the fields
+			else:			
+				valDict[group][m].append(dictName[year][entry][m])
+	return valDict
+
+def heatRankInp(dictName, year):
+	from operator import itemgetter
+	dataPrelim = dict()
+	dataPick = dict()
+	inpComb = []
+	for title in dictName[year].keys():
+		if 'actual' in dictName[year][title]['boxOffice'].keys():
+			box = dictName[year][title]['boxOffice']['actual'][-1]
+		else:
+			box = 0
+		dataPrelim[title]=box
+		dataPick[title]=dictName[year][title]['overallPick']
+	r = {key: rank for rank, key in enumerate(sorted(set(dataPrelim.values()), 
+		reverse=True), 1)}
+	t = {k: r[v] for k,v in dataPrelim.items()}
+	for entry,pick in dataPick.items():
+		rank = t[entry]
+		pickL = str(pick)
+		inpComb.append([entry,pick,rank,pickL,' '])
+	inpSorted = sorted(inpComb, key=itemgetter(1))
+	return zip(*inpSorted)
+
+#return a dictionary that contains a list of titles within a segment
+def getPicks(segment, dictName, year):
+	groupDict = getUniq(segment, dictName, year, defval='list')
+	for entry in dictName[year].keys():
+		group = dictName[year][entry][segment]
+		if group != 'na':
+			groupDict[group].append(entry)
+	return groupDict
+
+#return user specific cumulative time series
+def userCume(dictName, year, segment='id', month=1, day=1):
+	import datetime
+	import numpy as np
+	cumeDict = getUniq(segment, dictName, year, defval='dict')
+	titleDict = getPicks(segment, dictName, year)
+	schedDict = dict()
+	for entry in dictName[year]:
+		rd = dictName[year][entry]['releaseDate']
+		rdF = datetime.datetime.strptime(rd, '%d-%b-%y').date()
+		schedDict[entry] = rdF
+	start = datetime.date(year, month, day)
+	end = datetime.date(year+1, 1, 1)
+	for pid in cumeDict:
+		grossCume = 0
+		for n in range(int ((end - start).days)):
+			label = ''
+			day = (start + datetime.timedelta(n))
+			if len(titleDict[pid])>0:
+				for title in titleDict[pid]:
+					if schedDict[title] == day:
+						if 'act' in dictName[year][title]['box'].keys():
+							box = dictName[year][title]['box']['act']/1000000
+							label = title+': '+str(box)+' (actual)'
+						elif 'delayed' in dictName[year][title].keys():
+							box = 0
+							label = title+': Delayed'
+						elif 'bop' in dictName[year][title]['box'].keys():
+							box = dictName[year][title]['box']['bop']/1000000
+							label = title+': '+str(box)+' (BOP proj.)'
+						else:
+							box = np.mean([dictName[year][title]['box']['al'], 
+								dictName[year][title]['box']['tim']])/1000000
+							label = title+': '+str(box)+' (Expert proj.)'
+						grossCume = grossCume + box
+						titleDict[pid].remove(title)
+			cumeDict[pid][day] = [grossCume,label]
+
+	return cumeDict
